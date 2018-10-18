223a4575750918d76f14fa4f1a175b8e50a3a10e
willmonahan <willmonahan13@gmail.com>
2018-10-18 18:10:50 -0400

    moved molecules object array (for demo) into a separate js file

---
+++ demo/molecules.js
@@ -0,0 +1,44 @@
+var molecules = [
+    {
+        name: "molxr",
+    },
+    {
+        name: "test",
+    },
+    {
+        name: "caffeine",
+    },
+    {
+        name: "oxycontin",
+    },
+    {
+        name: "hydrocortisone",
+    },
+    {
+        name: "penicillin",
+    },
+    {
+        name: "ethanol",
+    },
+    {
+        name: "water",
+    },
+    {
+        name: "methane",
+    },
+    {
+        name: "ammonia",
+    },
+    {
+        name: "carbon_dioxide",
+    },
+    {
+        name: "n-pentane",
+    },
+    {
+        name: "two-methylbutane",
+    },
+    {
+        name: "two-two-dimethylpropane",
+    },
+];
